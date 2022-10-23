2262f07562e2fe4dc625db452d9918c9dc6ea52b
Alex_Urtubia <alex.urtubia.contreras@gmail.com>
2022-10-23 02:12:13 -0300

    first commit

---
+++ src/components/Contador.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+
+function Contador () {
+
+    const [cuenta, setCuenta] = useState(100)
+
+    const aumentar = () => {
+        setCuenta( cuenta + 1)
+    };
+
+    const disminuir = () => {
+        setCuenta(cuenta - 1)
+    };
+
+    const reset = () => {
+        setCuenta(100)
+    };  
+    
+    useEffect(() => {
+        console.log('El contador va en',cuenta)
+    }, [cuenta])
+
+    return ( 
+    <div> 
+        <center>
+            <h1>{cuenta}</h1> 
+            <button onClick={aumentar}>Aumentar</button> 
+            <button onClick={disminuir}>Disminuir</button>
+            <button onClick={reset}>Reset</button>
+        </center> 
+    </div>
+        )
+    }
+
+export default Contador;
+
+
+
